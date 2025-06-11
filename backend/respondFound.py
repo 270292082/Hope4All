dadb45ee9808751d96f89f987a817646b5387843
270292082 <tomwongs.yoobee@gmail.com>
2025-06-11 15:40:36 +1200

    Fixing the communication between the DB and respond-found page.

--- backend/respondFound.py
+++ backend/respondFound.py
@@ -13,16 +13,16 @@ def found(MID):
 
     conn = db_connection()
     missingPerson = conn.execute('SELECT * FROM Missing WHERE MID = ?', (MID,)).fetchone()
-    print(missingPerson)
     rescuer = conn.execute('SELECT * FROM Rescuer WHERE Email = ?', (session['userEmail'],)).fetchone()
 
     if request.method == 'POST':
-        foundLocation =request.form('foundLocation')
-        foundDate = request.form('foundDate')
-        foundCondition = request.form('foundCondition')
-        foundContact = request.form('foundContact')
+        foundLocation = request.form['foundLocation']
+        foundDate = request.form['foundDate']
+        foundCondition = request.form['foundCondition']
+        foundContact = request.form['foundContact']
 
-        reporterName = rescuer['FullName'] 
+        print("RESCUER:\n",rescuer)
+        reporterName = rescuer['FirstName'] + " " + rescuer["LastName"] 
         reporterRole = rescuer['Role'] 
         RId = rescuer['RID']
 
@@ -33,7 +33,7 @@ def found(MID):
         conn.commit()
         conn.close()
         flash('Report submitted successfully.')
-        return redirect(url_for('homepage'))
+        return redirect('/')
     
     conn.close()
 
