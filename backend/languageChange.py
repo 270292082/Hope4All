d516ed2b98065924b238de5b87a883d6ad9e2c2a
thaesu9 <blossomthaesu@gmail.com>
2025-06-18 02:29:40 +1200

    language solved, information added, missing person page ui fix

--- backend/languageChange.py
+++ backend/languageChange.py
@@ -12,6 +12,7 @@ def get_locale():
     lang = session.get('language')
     if lang in LANGUAGES:
         return lang
+    #print(lang)
     return request.accept_languages.best_match(LANGUAGES.keys())
 
 
